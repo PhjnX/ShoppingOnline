1bc6e8b875bb706485064f091c67cb59372b5ba5
BUI HOANG PHI <127970845+PhjnX@users.noreply.github.com>
2024-07-23 01:02:46 +0700

    Add files via upload
    Upload server

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: "cluster.6zflivl.mongodb.net",
+  DB_USER: "phongx2003",
+  DB_PASS: "hoangphi1305",
+  DB_DATABASE: "OnlineShopping",
+  EMAIL_USER: "buihoangphi2003@gmail.com", // Microsoft mail service
+  EMAIL_PASS: "mutedhfulcgtojoi",
+  JWT_SECRET: "KHOA_BI_MAT",
+  JWT_EXPIRES: "123898138191", // in milliseconds
+};
+module.exports = MyConstants;
